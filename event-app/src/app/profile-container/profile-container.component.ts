bbd72198d72526460f1423ed4676672b7ca67bd7
Jessie Luo <jessie.luo@utdallas.edu>
2021-08-11 19:14:22 -0500

    Add initial layout to profile page.

--- event-app/src/app/profile-container/profile-container.component.ts
+++ event-app/src/app/profile-container/profile-container.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProfileContainerComponent implements OnInit {
   @Input() name: string;
+  @Input() email: string;
 
   constructor() { }
 
